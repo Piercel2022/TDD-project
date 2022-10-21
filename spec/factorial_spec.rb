e988fdc2fcb0502ee32dd92c82f9907edae58fe1
looh16 <langacustodio@gmail.com>
2022-10-21 15:14:16 +0200

    Add factorial method test

---
+++ spec/factorial_spec.rb
@@ -0,0 +1,12 @@
+require './solvers'
+
+describe Solver do
+    context 'testing solvers class methods' do
+      it 'Should return 120 when number is 5' do
+        s = Solver.new
+        result = s.factorial(5)
+        expect(result).to eq 120
+      end
+    end
+  end
+
