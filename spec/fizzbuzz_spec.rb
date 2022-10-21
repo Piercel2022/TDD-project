4dd77af67a8ed7357a14c5c1094c4d790be87ebe
Pierre <piercel.coder2022@gmail.com>
2022-10-21 19:00:12 +0200

    Add the fizzbuzz specification

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,26 +1,28 @@
 require './solvers'
 
 describe Solver do
-    context 'Test the fizzbuzz method in Solver Class' do
-        it 'should return fizzbuzz if 15 is passed as number' do
-            solver = Solver.new
-          string = solver.fizzbuzz(15)
-          expect(string).to eql 'fizzbuzz'
-        end
-    
-        it 'should return fizz if 3 and 5 is passed as number' do
-          string = solver.fizzbuzz(3)
-          expect(string).to eql 'fizz'
-        end
-    
-        it 'should return buzz if 5 is passed as number' do
-          string = solver.fizzbuzz(5)
-          expect(string).to eql 'buzz'
-        end
-    
-        it 'should return 7 as a string it is passed as number' do
-          string = solver.fizzbuzz(7)
-          expect(string).to eql '7'
-        end
+  context 'Test the fizzbuzz method in Solver Class' do
+    describe 'fizzbuzz' do
+      it 'returns "fizz" when the number is divisible by 3' do
+        string = fizzbuzz(6)
+
+        expect(string).to eq('fizz')
+      end
+      it 'returns "buzz" when the number is divisible by 5' do
+        string = fizzbuzz(10)
+
+        expect(string).to eq('buzz')
+      end
+      it 'returns "fizzbuzz" when the number is divisible by 3 and 5' do
+        string = fizzbuzz(15)
+
+        expect(string).to eq('fizzbuzz')
+      end
+      it 'returns "7" in other case' do
+        string = fizzbuzz(4)
+
+        expect(string).to eq('7')
+      end
+    end
   end
 end
