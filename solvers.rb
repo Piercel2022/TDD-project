a7f185d0085faf7a4d9967419c75e821576f254b
looh16 <langacustodio@gmail.com>
2022-10-21 15:17:11 +0200

    Add factorial method and fix test error

--- solvers.rb
+++ solvers.rb
@@ -1,3 +1,11 @@
 class Solver
-    
+  def factorial(num)
+    if num.negative?
+      'Number need to be positive'
+    elsif num == 1
+      1
+    else
+      num * factorial(num - 1)
+    end
   end
+end
--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -1,12 +1,11 @@
 require './solvers'
 
 describe Solver do
-    context 'testing solvers class methods' do
-      it 'Should return 120 when number is 5' do
-        s = Solver.new
-        result = s.factorial(5)
-        expect(result).to eq 120
-      end
+  context 'testing solvers class methods' do
+    it 'Should return 120 when number is 5' do
+      s = Solver.new
+      result = s.factorial(5)
+      expect(result).to eq 120
     end
   end
-
+end
