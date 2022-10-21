170d3892fb3bab394b898d166ff93dad9b8eae46
Pierre <piercel.coder2022@gmail.com>
2022-10-21 19:45:31 +0200

    Add the required changes

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,24 +4,28 @@ describe Solver do
   context 'Test the fizzbuzz method in Solver Class' do
     describe 'fizzbuzz' do
       it 'returns "fizz" when the number is divisible by 3' do
-        string = fizzbuzz(6)
+        s = Solver.new
+        string = s.fizzbuzz(6)
 
-        expect(string).to eq('fizz')
+        expect(string).to eq 'fizz'
       end
       it 'returns "buzz" when the number is divisible by 5' do
-        string = fizzbuzz(10)
+        s = Solver.new
+        string = s.fizzbuzz(10)
 
-        expect(string).to eq('buzz')
+        expect(string).to eq 'buzz'
       end
       it 'returns "fizzbuzz" when the number is divisible by 3 and 5' do
-        string = fizzbuzz(15)
+        s = Solver.new
+        string = s.fizzbuzz(15)
 
-        expect(string).to eq('fizzbuzz')
+        expect(string).to eq 'fizzbuzz'
       end
       it 'returns "7" in other case' do
-        string = fizzbuzz(4)
+        s = Solver.new
+        string = s.fizzbuzz(4)
 
-        expect(string).to eq('7')
+        expect(string).to eq '7'
       end
     end
   end
